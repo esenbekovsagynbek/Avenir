c69cdd5bab908228d93538f456885f780a8d6cda
Sagynbek Esenbekov <jagger773@gmail.com>
2021-04-20 19:18:03 +0600

    second commit

---
+++ src/pages/Second.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>Second page</h2>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem officia sapiente sunt rerum, quod blanditiis ex ullam fugit consectetur nesciunt?</p>
+        <router-link to="/">Home page</router-link>
+    </div>
+</template>
